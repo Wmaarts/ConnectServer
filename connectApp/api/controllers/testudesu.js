c59b5ec06453e3e41f72178a0164b2c0f8fd0350
mjowned <mj65@live.nl>
2017-02-28 16:59:31 +0100

    working 2nd controller

---
+++ connectApp/api/controllers/testudesu.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var util = require('util');
+
+module.exports = {
+  hiya: hiya
+};
+
+function hiya(req, res) {
+  // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
+  var name = req.swagger.params.name.value || 'stranger';
+  var hello = util.format('Hello, %s!', name);
+
+  // this sends back a JSON response which is a single string
+  res.json(hello);
+}

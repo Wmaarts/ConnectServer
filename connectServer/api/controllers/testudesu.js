e8634d127563332c33db8df6f47cf5afdadad137
mjowned <mj65@live.nl>
2017-03-24 11:25:29 +0100

    backup in case I do something stupid

--- connectServer/api/controllers/testudesu.js
+++ connectServer/api/controllers/testudesu.js
@@ -3,13 +3,23 @@
 var util = require('util');
 
 module.exports = {
-  hiya: hiya
+  hiya: hiya,
+  hiyas: test
 };
 
 function hiya(req, res) {
   // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
   var name = req.swagger.params.name.value || 'stranger';
   var hello = util.format('Hello, %s!', name);
+
+  // this sends back a JSON response which is a single string
+  res.json(hello);
+}
+
+function test(req, res) {
+  // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
+  var name = req.swagger.params.name.value || 'stranger';
+  var hello = util.format('Diff, %s!', name);
 
   // this sends back a JSON response which is a single string
   res.json(hello);

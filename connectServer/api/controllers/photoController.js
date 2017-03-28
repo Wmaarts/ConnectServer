8bcc47ef4e7d8056bd981b2d23f25a91eb1a0c61
mjowned <mj65@live.nl>
2017-03-28 11:44:37 +0200

    delete func working + refactoring

--- connectServer/api/controllers/photoController.js
+++ connectServer/api/controllers/photoController.js
@@ -1,50 +1,32 @@
-var handleError;
+var handleError; //TODO error handling
 
 var util = require('util');
 var mongoose = require('mongoose');
 Photo = mongoose.model('Photo');
 
 module.exports = {
-  postPhoto: savePhoto,
+  postPhoto: createPhoto,
   getPhoto: getPhoto,
+  deletePhoto: deletePhoto,
 };
 
-// POST
-function savePhoto(req, res) { //TODO make this work
-    console.log("we are inside!");
-    console.log(req.body);
-
+// CREATE (POST)
+function createPhoto(req, res) {
     var photo = new Photo(req.body);
-	console.log(photo);
 
 	photo.save()
 		.then(savedPhoto => {
-			console.log("inside saved photo code");
 			// console.log("err: "+err); // err makes the code crash derp
-
-			// if(err){ // if it got inside this part of the code, probs no errors? 
-			// 	console.log("error");
-			// 	return handleError(req, res, 500, err); 
-			// }
-			// else {
-				// res.status(201);
-				// res.json(savedPhoto);
-				// console.log("response json")
-				// res.json({success: 1, description: "Photo added to the list!"});
-			// }
-
-			console.log("response json")
 
 			// TODO -> add the photo to a service
 
-			res.json({success: 1, description: "Photo added to the list!"});
-		}).fail(err => handleError(req, res, 500, err));
-
-
+			res.json({success: 1, description: "Photo added"});
+		})
+		.fail(err => handleError(req, res, 500, err));
 }
 
+// READ (GET)
 function getPhoto(req, res) {
-	console.log("inside getPhoto");
 	var query = {};
 
 	//req.swagger contains the path parameters
@@ -56,13 +38,22 @@ function getPhoto(req, res) {
 			// Don't return an array, return the element
 			if(req.swagger.params.id){
 				data = data[0];
-				console.log("data: "+data);
 			}
-			return res.json(data);
+			return res.json(data); // return?
 		})
 		.fail(err => handleError(req, res, 500, err));
 }
 
+// DELETE (DELETE)
 function deletePhoto(req, res, next) {
+	var query = {};
 
+	query._id = req.swagger.params.id.value; //note _id -> query has to search on the correct name, no misspellings
+
+	var photoResult = Photo.findByIdAndRemove(query);
+
+	photoResult.then(data => {
+		res.json({success: 1, description: "Photo deleted"});
+	})
+	.fail(err => handleError(req, res, 500, err));
 }
